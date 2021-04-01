dbd953b464759b2e5a74a8c500b67c0eaeb2d022
BlackCyber21 <rafael_for.work@hotmail.com>
2021-04-01 01:10:46 -0300

    Initial Commit

---
+++ utils.py
@@ -0,0 +1,27 @@
+from models import Pessoas
+
+def insere_pessoas():
+    pessoa = Pessoas(nome='Fernando',idade=25)
+    print(pessoa)
+    pessoa.save()
+
+def consulta_pessoas():
+    pessoas = Pessoas.query.all()
+    print(pessoas)
+    pessoa = Pessoas.query.filter_by(nome='Rafael').first()
+    print(pessoa.idade)
+
+def altera_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Fernando').first()
+    pessoa.nome = 'Jose'
+    pessoa.save()
+
+def exclui_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Fernando').first()
+    pessoa.delete()
+
+if __name__ == '__main__':
+    #insere_pessoas()
+    #altera_pessoa()
+    exclui_pessoa()
+    consulta_pessoas()
